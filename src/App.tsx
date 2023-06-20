087b6a7e931e88582142d0e9f60d9957ffd1d2ba
Brunodw123br <brunosilvaandradeDev@gmail.com>
2023-06-20 15:28:52 -0300

    inicio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    //div para colocar a cor no background
+    <div className="container">
+      /*container do usuario */
+      <div className="usuarioContainer">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
